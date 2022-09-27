d124d2127607d5bfb0db0860ed6c60a0a6ff0ec5
Dudnik.Pavel <Pavel.Dudnik@infotecs.ru>
2022-09-27 17:52:50 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"github.com/nats-io/nats.go"
+
+	"fmt"
+	"time"
+)
+
+func print_error(err error) {
+	fmt.Printf("err: %s\n", err.Error())
+}
+
+// Колбэк
+func cb(msg *nats.Msg) {
+	fmt.Printf("Async subscription: %s\n", string(msg.Data))
+}
+
+func main() {
+	const subject string = "test_subject"
+	const error_format string = "err: %s\n"
+	const timeout time.Duration = time.Second * 3
+
+	// Создание соединение с сервером
+	nc, err := nats.Connect(nats.DefaultURL)
+
+	if nil == nc {
+		print_error(err)
+		return
+	}
+
+	// Асинхронная nats подписка
+	{
+		// Создание асинхронной подписки
+		asyncSub, err := nc.Subscribe(subject, cb)
+
+		if nil != err {
+			print_error(err)
+			return
+		}
+
+		// Публикация данных для подписки
+		err = nc.Publish(subject, []byte("Some asynchronous published data"))
+
+		if nil != err {
+			print_error(err)
+			return
+		}
+
+		// Дизлайк, отписка
+		asyncSub.Unsubscribe()
+		// Закрытие соединения с подпиской при продолжающейся работе колбэков
+		asyncSub.Drain()
+	}
+
+	{
+		// Создание синхронной подписки
+		syncSub, err := nc.SubscribeSync(subject)
+
+		if nil != err {
+			print_error(err)
+			return
+		}
+
+		// Публикация данных для подписки
+		err = nc.Publish(subject, []byte("Some synchronous published data"))
+
+		if nil != err {
+			print_error(err)
+			return
+		}
+
+		// Чтение сообщения для подписки
+		msg, err := syncSub.NextMsg(timeout)
+
+		if nil != err {
+			print_error(err)
+			return
+		}
+
+		if nil != msg {
+			fmt.Printf("Sync subscription: %s\n", msg.Data)
+		}
+
+		syncSub.Unsubscribe()
+		syncSub.Drain()
+	}
+
+	// Отключает все подписки
+	nc.Drain()
+	// Закрытие соединения с сервером
+	nc.Close()
+}
